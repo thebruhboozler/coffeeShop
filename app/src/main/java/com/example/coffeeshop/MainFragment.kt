86a9d1c859cf0a113b5558de04425f60221a2a31
THEBU <THEBU@GAGA_QANAVA>
2025-04-15 21:22:37 +0400

    implemented second design

--- app/src/main/java/com/example/coffeeshop/MainFragment.kt
+++ app/src/main/java/com/example/coffeeshop/MainFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 
 
 class MainFragment : Fragment() {
+
+
+    private var _binding: MainFragment? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +22,18 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val orderButton: AppCompatButton = view.findViewById(R.id.OrderButton)
+        orderButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer,finishFragment())
+                .commit()
+        }
+    }
+
 
     companion object {
 
---
+++ app/src/main/java/com/example/coffeeshop/finishFragment.kt
@@ -0,0 +1,27 @@
+package com.example.coffeeshop
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class finishFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_finish, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            finishFragment().apply {
+            }
+    }
+}
